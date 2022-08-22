c62e695266d8c3ea70142eef2330ace41f0af27f
spcn2768 <109647227+spcn2768@users.noreply.github.com>
2022-08-22 15:44:24 +0900

    Delete js directory

--- js/move.js
+++
@@ -1,7 +0,0 @@
-// 페이지 이동 함수
-const moveTo = (prev, next) => {
-  const prevPage = document.getElementById(prev);
-  const nextPage = document.getElementById(next);
-  prevPage.style.display = "none";
-  nextPage.style.display = "block";
-};
